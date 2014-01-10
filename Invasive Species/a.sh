2cbf436db7a0c8aefb6b383b944fc076f2939fc7
Philipp Beau <PhilippB3au@gmail.com>
2014-01-10 14:32:35 +0100

    Added mac version, broke windows version

---
+++ Invasive Species/a.sh
@@ -0,0 +1,4 @@
+pwd
+/usr/local/bin/rl_glue &
+//anaconda/bin/python -u python/InvasiveExperiment.py &
+//anaconda/bin/python python/InvasiveEnvironment.py
---
+++ Invasive Species/src/nl/uva/species/InitialiserMac.java
@@ -0,0 +1,5 @@
+package nl.uva.species;
+
+public class InitialiserMac {
+
+}

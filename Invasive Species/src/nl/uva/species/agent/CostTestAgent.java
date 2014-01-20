e0c6be458fb8ea8b4c66c9aa965f9744f845cd89
lusl <luisa.zintgraf@hotmail.de>
2014-01-20 10:29:10 +0100

    Finished CostSolver and CostTestAgent

--- Invasive Species/src/nl/uva/species/agent/CostTestAgent.java
+++ Invasive Species/src/nl/uva/species/agent/CostTestAgent.java
@@ -1,13 +1,11 @@
 package nl.uva.species.agent;
 
 import java.util.Arrays;
-
-import javax.swing.JFrame;
+import java.util.Random;
 
 import nl.uva.species.model.EnvModel;
 import nl.uva.species.model.River;
 import nl.uva.species.model.RiverState;
-import nl.uva.species.ui.GraphInterface;
 import nl.uva.species.utils.CostSolver;
 import nl.uva.species.utils.Utilities;
 
@@ -19,15 +17,20 @@ import org.rlcommunity.rlglue.codec.util.AgentLoader;
 
 public class CostTestAgent implements AgentInterface {
 
-    private boolean policyFrozen;
-    private boolean exploringFrozen;
-    private River river;
-
+    private River mRiver;
+    private EnvModel mModel;
+    private CostSolver mCostSolver;
+    
+    // the last state and action, which the reward depends on
     private RiverState mLastState;
+    private Action mLastActions;
+    
+    private Random mRand;
+    private double mBadActionPenalty;
     
     @Override
     public void agent_init(final String taskSpecification) {
-        System.out.println(taskSpecification);
+        // System.out.println(taskSpecification);
         TaskSpec theTaskSpec = new TaskSpec(taskSpecification);
 
         // Assume the message is valid
@@ -46,100 +49,89 @@ public class CostTestAgent implements AgentInterface {
 
         System.err.println("budget " + Double.parseDouble(theExtra[1].split("by")[0]));
 
-        river = new River(theTaskSpec);
+        mRiver = new River(theTaskSpec);
+        
+        // added this to the init method
+        mRand = new Random();
+        mModel = new EnvModel(mRiver);
+        mCostSolver = new CostSolver(mModel);
+        
     }
 
     @Override
     public Action agent_start(final Observation observation) {
-
+                
+        System.out.println("START");
+        
         Action defaultAction = new Action();
         defaultAction.intArray = new int[7];
         Arrays.fill(defaultAction.intArray, Utilities.ACTION_ERADICATE_RESTORE);
-
-        EnvModel model = new EnvModel(river);
-        RiverState state = new RiverState(river, observation);
-        mLastState = state;
-
-        // System.out.println("Agent_Start: " + observation.intArray.length);
-
         int[] theState = new int[7];
         Action returnAction = new Action();
-
         for (int i = 0; i < 7; ++i) {
             theState[i] = Utilities.ACTION_NOTHING;
             int tam = 0;
             int empty = 0;
-
             for (int j = 0; j < 4; ++j) {
-                if (observation.intArray[i * 4 + j] == Utilities.HABITAT_INVADED) {
-                    ++tam;
-                }
-                if (observation.intArray[i * 4 + j] == Utilities.HABITAT_EMPTY) {
-                    ++empty;
-                }
+                if (observation.intArray[i * 4 + j] == Utilities.HABITAT_INVADED) ++tam;
+                if (observation.intArray[i * 4 + j] == Utilities.HABITAT_EMPTY) ++empty;
             }
-            if (tam >= 1) {
-                theState[i] = Utilities.ACTION_ERADICATE_RESTORE;
-            } else if (empty >= 1) {
-                theState[i] = Utilities.ACTION_RESTORE;
-            }
+            double rnd = mRand.nextDouble();
+            if (tam >= 1 && rnd < 0.4) theState[i] = Utilities.ACTION_ERADICATE_RESTORE;
+            else if (tam >= 1 && rnd < 0.7) theState[i] = Utilities.ACTION_ERADICATE;
+            else if (empty >= 1) theState[i] = Utilities.ACTION_RESTORE;
         }
 
         returnAction.intArray = theState;
-
+        
+        // update last state and action
+        mLastState = new RiverState(mRiver, observation);  
+        mLastActions = returnAction;
+        
         return returnAction;
     }
 
     @Override
     public Action agent_step(final double reward, final Observation observation) {
+                             
+        RiverState state = new RiverState(mRiver, observation);
+        
         Action defaultAction = new Action();
         defaultAction.intArray = new int[7];
         Arrays.fill(defaultAction.intArray, Utilities.ACTION_ERADICATE_RESTORE);
-
-        EnvModel model = new EnvModel(river);
-        RiverState state = new RiverState(river, observation);
-
-        // System.out.println("Agent_Start: " + observation.intArray.length);
-
         int[] theState = new int[7];
         Action returnAction = new Action();
-
         for (int i = 0; i < 7; ++i) {
             theState[i] = Utilities.ACTION_NOTHING;
             int tam = 0;
             int empty = 0;
-
             for (int j = 0; j < 4; ++j) {
-                if (observation.intArray[i * 4 + j] == Utilities.HABITAT_INVADED) {
-                    ++tam;
-                }
-                if (observation.intArray[i * 4 + j] == Utilities.HABITAT_EMPTY) {
-                    ++empty;
-                }
+                if (observation.intArray[i * 4 + j] == Utilities.HABITAT_INVADED) ++tam;
+                if (observation.intArray[i * 4 + j] == Utilities.HABITAT_EMPTY) ++empty;
             }
-            if (tam >= 1) {
-                theState[i] = Utilities.ACTION_ERADICATE_RESTORE;
-            } else if (empty >= 1) {
-                theState[i] = Utilities.ACTION_RESTORE;
-            }
+            double rnd = mRand.nextDouble();
+            if (tam >= 1 && rnd < 0.4) theState[i] = Utilities.ACTION_ERADICATE_RESTORE;
+            else if (tam >= 1 && rnd < 0.8) theState[i] = Utilities.ACTION_ERADICATE;
+            else if (empty >= 1) theState[i] = Utilities.ACTION_RESTORE;
         }
+
+        // if the cost parameters haven't been found yet, add the new data to the solver
+        if (!mCostSolver.costParametersFound() && reward != mBadActionPenalty) {
+            mCostSolver.addData(mLastState, mLastActions, reward);
+         }
 
         returnAction.intArray = theState;
-
-        CostSolver costSolver = new CostSolver(model);
-        if (!costSolver.costParametersFound()) {
-            costSolver.addData(mLastState, returnAction, reward);
-        }
         
+        // update last state and action
         mLastState = state;
-        
+        mLastActions = returnAction;
+
         return returnAction;
     }
 
     @Override
     public void agent_end(final double reward) {
         System.out.println("Agent_End");
-
     }
 
     @Override
@@ -157,7 +149,6 @@ public class CostTestAgent implements AgentInterface {
         // Action{ Set flag to stop updating policy
         //
         if (inMessage.startsWith("freeze learning")) {
-            this.policyFrozen = true;
             return "message understood, policy frozen";
         }
         // Message Description
@@ -165,7 +156,6 @@ public class CostTestAgent implements AgentInterface {
         // Action{ Set flag to resume updating policy
         //
         if (inMessage.startsWith("unfreeze learning")) {
-            this.policyFrozen = false;
             return "message understood, policy unfrozen";
         }
         // Message Description
@@ -173,7 +163,6 @@ public class CostTestAgent implements AgentInterface {
         // Action{ Set flag to stop exploring (greedy actions only)
         //
         if (inMessage.startsWith("freeze exploring")) {
-            this.exploringFrozen = true;
             return "message understood, exploring frozen";
         }
         // Message Description
@@ -181,7 +170,6 @@ public class CostTestAgent implements AgentInterface {
         // Action{ Set flag to resume exploring (e-greedy actions)
         //
         if (inMessage.startsWith("unfreeze exploring")) {
-            this.exploringFrozen = false;
             return "message understood, exploring frozen";
         }
         return "Invasive agent does not understand your message.";
--- Invasive Species/src/nl/uva/species/utils/CostSolver.java
+++ Invasive Species/src/nl/uva/species/utils/CostSolver.java
@@ -6,6 +6,7 @@ import org.apache.commons.math3.linear.DecompositionSolver;
 import org.apache.commons.math3.linear.LUDecomposition;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealVector;
+import org.apache.commons.math3.linear.SingularValueDecomposition;
 
 import nl.uva.species.model.EnvModel;
 import nl.uva.species.model.Reach;
@@ -15,102 +16,123 @@ import org.rlcommunity.rlglue.codec.types.Action;
 /**
  * Solves the cost parameters
  * 
- * Should be used like this:
- * if (!costParametersFound()) {
- *      addData(RiverState state, Action actions, reward)
- * }
+ * Should be used like this: if (!costParametersFound() && reward != BadActionPenalty) { addData(RiverState state,
+ * Action actions, reward) }
  */
 public class CostSolver {
-    
+
     /** The model for which the cost parameters should be set for */
     private EnvModel mModel;
-    
+
     /** Indicator whether the cost parameters are found already */
     private boolean mCostParametersFound;
-    
+
     /** The number of (cost) parameters which we want to estimate */
     private int mNumberOfVariables;
-    
-    /** Matrix for left side of the equation 
-     * The columns are 
-     * (#invadedHabitatsTotal, #emptyHabitatsTotal, #invadedReaches, 
-     *  #eradicatedReaches, #restoredReaches+#eradicatedAndRestoredReaches, 
-     *  #eradticatedTreesTotal, #restoredTreesTotal, #eradicatedAndRestoredTreesTotal).
+
+    /**
+     * Matrix for left side of the equation The columns are (#invadedHabitatsTotal, #emptyHabitatsTotal,
+     * #invadedReaches, #eradicatedReaches, #restoredReaches+#eradicatedAndRestoredReaches, #eradticatedTreesTotal,
+     * #restoredTreesTotal, #eradicatedAndRestoredTreesTotal).
      * */
     private RealMatrix mDataMatrix;
-    
+
     /** Vector for right side of the equation, each entry holds a reward */
     private RealVector mDataVector;
-    
-    /** The vector that holds the cost parameters, if found*/
+
+    /** The vector that holds the cost parameters, if found */
     private RealVector mCostParameters;
-    
+
     /** Indicator at which position we want to fill the matrix currently */
     private int mFillPosition;
-    
+
     public CostSolver(final EnvModel model) {
         //
         mModel = model;
         mCostParametersFound = false;
         mFillPosition = 0;
         mNumberOfVariables = 8;
-        
+
         // create the matrix and the vector in which the equations will be stored
         mDataMatrix = new Array2DRowRealMatrix(mNumberOfVariables, mNumberOfVariables);
         mDataVector = new ArrayRealVector(mNumberOfVariables);
     }
- 
+
     /**
-     * Checks if the new data is usable for the calculations and if so, adds them.
-     * If enough data is collected, this solves the linear system
+     * Checks if the new data is usable for the calculations and if so, adds them. If enough data is collected, this
+     * solves the linear system
      * 
      * @param state
      * @param actions
      * @param reward
      */
     public void addData(final RiverState state, final Action actions, final double reward) {
-        
+
         // we will only add data if the matrix is not full yet
-        if (mFillPosition > mNumberOfVariables) {
+        if (mFillPosition == mNumberOfVariables) {
             return;
         }
-        
+
         // get the data we need for calculations
         RealVector newDataRow = fillNewDataRow(state, actions);
-        
-        // go through the rows which are already filled and check if the new data is linearly dependent to one of them
+
+        // if the vector has zero norm, we can't use it
+        if (newDataRow.getNorm() == 0) {
+            return;
+        }
+
         boolean insert = true;
-        // copy and unitize new vector
-        RealVector unitizedNewDataRow = newDataRow.copy();
-        unitizedNewDataRow.unitize();
-        for (int i = 0; i < mFillPosition; ++i) {
-            // copy and unitize current matrix row
-            RealVector unitizedDataMatrixRow = mDataMatrix.getRowVector(i);
-            unitizedDataMatrixRow.unitize();
-            // if they are equal, the vectors are linear dependent and we don't want to add the new data
-            if (unitizedNewDataRow.equals(unitizedDataMatrixRow)) {
+        if (mFillPosition != 0) {
+            // check if new data row is linear dependent on the already filled up rows of the matrix
+            RealMatrix subMatrix = mDataMatrix.getSubMatrix(0, mFillPosition, 0, mNumberOfVariables - 1);
+            subMatrix.setRowVector(mFillPosition, newDataRow);
+            SingularValueDecomposition decomp = new SingularValueDecomposition(subMatrix);
+            if (decomp.getRank() < mFillPosition + 1) {
                 insert = false;
-                break;
             }
         }
-        
+
         // if the new data isn't linear dependent on the old data, we can add it to the matrix and the vector
         if (insert) {
+            System.out.println("Fillposition: ");
+            System.out.println(mFillPosition);
             mDataMatrix.setRowVector(mFillPosition, newDataRow);
             mDataVector.setEntry(mFillPosition, reward);
             mFillPosition++;
+
+            // print out details about this round (observation/action/dataRow)
+            System.out.println(" ");
+            System.out.print("Observation: ");
+            for (int i = 0; i < state.getObservation().intArray.length; ++i) {
+                System.out.print(state.getObservation().intArray[i]);
+                System.out.print(" ");
+            }
+            System.out.println(" ");
+            System.out.print("Action: ");
+            for (int i = 0; i < actions.intArray.length; ++i) {
+                System.out.print(actions.intArray[i]);
+                System.out.print(" ");
+            }
+            System.out.println(" ");
+            System.out.print("Reward: ");
+            System.out.print(reward);
+            System.out.println(" ");
+            System.out.print("New data row: ");
+            for (int i = 0; i < newDataRow.getDimension(); ++i) {
+                System.out.print(newDataRow.getEntry(i));
+                System.out.print(" ");
+            }
         }
-        
+
         // if we have gained enough data, then we can solve the system of linear equations
-        if (mFillPosition > mNumberOfVariables) {
+        if (mFillPosition == mNumberOfVariables) {
             solveCostParameters();
         }
     }
-    
+
     /**
-     * Solves the linear equation given with the data matrix and data vector,
-     * sets the cost parameters in the model
-     * and sets cost parameters to true
+     * Solves the linear equation given with the data matrix and data vector, sets the cost parameters in the model and
+     * sets cost parameters to true
      * 
      * Should only be called once!
      */
@@ -119,7 +141,11 @@ public class CostSolver {
         mCostParameters = solver.solve(mDataVector);
         mModel.setCostParameters(mCostParameters);
         mCostParametersFound = true;
-        
+
+        System.out.println(mDataMatrix);
+        System.out.println(mDataVector);
+        System.out.println(mCostParameters);
+
         System.out.println(mCostParameters.getEntry(0));
         System.out.println(mCostParameters.getEntry(1));
         System.out.println(mCostParameters.getEntry(2));
@@ -128,21 +154,20 @@ public class CostSolver {
         System.out.println(mCostParameters.getEntry(5));
         System.out.println(mCostParameters.getEntry(6));
         System.out.println(mCostParameters.getEntry(7));
-        
+
     }
-    
+
     /**
-     * Takes a state and an action and fills a row vector with
-     * (#invadedHabitatsTotal, #emptyHabitatsTotal, #invadedReaches, 
-     *  #eradicatedReaches, #restoredReaches+#eradicatedAndRestoredReaches, 
-     *  #eradticatedTreesTotal, #restoredTreesTotal, #eradicatedAndRestoredTreesTotal).
-     *  
+     * Takes a state and an action and fills a row vector with (#invadedHabitatsTotal, #emptyHabitatsTotal,
+     * #invadedReaches, #eradicatedReaches, #restoredReaches+#eradicatedAndRestoredReaches, #eradticatedTreesTotal,
+     * #restoredTreesTotal, #eradicatedAndRestoredTreesTotal).
+     * 
      * @param state
      * @param actions
      * @return
      */
     private RealVector fillNewDataRow(final RiverState state, final Action actions) {
-        
+
         RealVector newDataRow = new ArrayRealVector(mNumberOfVariables);
 
         // state-dependent
@@ -156,7 +181,7 @@ public class CostSolver {
         int numberOfEradicatedTrees = 0;
         int numberOfRestoredTrees = 0;
         int numberOfEradicatedAndRestoredTrees = 0;
-        
+
         for (final Reach reach : state.getReaches()) {
             // state-dependent
             numberOfInvadedHabitats += reach.getHabitatsInvaded();
@@ -179,21 +204,21 @@ public class CostSolver {
                     break;
             }
         }
-        
+
         // state-dependent
-        newDataRow.setEntry(0, numberOfInvadedHabitats);
-        newDataRow.setEntry(1, numberOfEmptyHabitats);
-        newDataRow.setEntry(2, numberOfInvadedReaches);
+        newDataRow.setEntry(0, (double) numberOfInvadedHabitats);
+        newDataRow.setEntry(1, (double) numberOfEmptyHabitats);
+        newDataRow.setEntry(2, (double) numberOfInvadedReaches);
         // action-dependent
-        newDataRow.setEntry(3, numberOfEradicatedReaches);
-        newDataRow.setEntry(4, numberOfRestoredReaches+numberOfEradicatedAndRestoredReaches);
-        newDataRow.setEntry(5, numberOfEradicatedTrees);
-        newDataRow.setEntry(6, numberOfRestoredTrees);
-        newDataRow.setEntry(7, numberOfEradicatedAndRestoredTrees);
-        
+        newDataRow.setEntry(3, (double) numberOfEradicatedReaches);
+        newDataRow.setEntry(4, (double) numberOfRestoredReaches + numberOfEradicatedAndRestoredReaches);
+        newDataRow.setEntry(5, (double) numberOfEradicatedTrees);
+        newDataRow.setEntry(6, (double) numberOfRestoredTrees);
+        newDataRow.setEntry(7, (double) numberOfEradicatedAndRestoredTrees);
+
         return newDataRow;
     }
-    
+
     /**
      * Returns whether the cost parameters were already found
      * 
@@ -202,6 +227,5 @@ public class CostSolver {
     public boolean costParametersFound() {
         return mCostParametersFound;
     }
-  
-    
+
 }
